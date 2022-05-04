ab6f4ad58599a602915b8c3809b1c09c08308e65
Ohyoungkeun <oh-jack@hanmail.net>
2022-05-04 16:13:24 +0900

    aa

--- web-drawing-tool/src/App.vue
+++ web-drawing-tool/src/App.vue
@@ -2,103 +2,55 @@
   <v-app>
     <v-app-bar
       app
-      color="#EEEEEE"
+      color="primary"
+      dark
     >
-      <div class="program_name">
-      Web Drawing Tool
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
       </div>
 
       <v-spacer></v-spacer>
-      
-      <div class="toolbars">
-      <input type = "text" name = "width" class = "width_height_box">
-      <v-icon>mdi-alpha-x</v-icon>
-      <input type = "text" name = "height" class = "width_height_box">
-      |
-      <v-icon>mdi-undo</v-icon>
-      <v-icon>mdi-redo</v-icon>
-      <v-icon>mdi-content-cut</v-icon>
-      <v-icon>mdi-content-paste</v-icon>
-      <v-icon>mdi-content-copy</v-icon>
-      <v-icon>mdi-delete</v-icon>
-      |
-      <v-icon>mdi-magnify-minus-outline</v-icon>
-      <input type = "text" name = "ratio" value = "100%" class = "canvas_ratio_box">
-      <v-icon>mdi-magnify-plus-outline</v-icon>
-      </div>
 
-      <v-spacer></v-spacer>
-
-      <div class="wrap">
-
-      <div class = "a">
       <v-btn
-        href="새로 만들기 링크"
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
         target="_blank"
         text
       >
-        <span class = "font1">새로 만들기</span>
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
-      </div>
-
-      <div class = "a">
-      <v-btn
-        href="불러오기 링크"
-        target="_blank"
-        text
-      >
-        <span class = "font1">불러오기</span>
-      </v-btn>
-      </div>
-
-      <div class = "a">
-      <v-btn
-        href="저장하기 링크"
-        target="_blank"
-        text
-      >
-        <span class = "font1">저장하기</span>
-      </v-btn>
-      </div>
-
-      <div class = "a">
-      <v-btn
-        href="내보내기 링크"
-        target="_blank"
-        text
-      >
-        <span class = "font1">내보내기</span>
-      </v-btn>
-      </div>
-
-      </div>
-
     </v-app-bar>
 
     <v-main>
-
-      <div class = "wrap">
-        <Menu_left/>
-        <Content/>
-        <Menu_right/>
-      </div>
-
+      <HelloWorld/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import Menu_left from './components/Menu-left.vue';
-import Menu_right from './components/Menu-right.vue';
-import Content from './components/Content-part.vue';
+import HelloWorld from './components/HelloWorld';
 
 export default {
-  name: 'Web-Drawing-Tool',
+  name: 'App',
 
   components: {
-    Menu_left,
-    Menu_right,
-    Content
+    HelloWorld,
   },
 
   data: () => ({
@@ -106,49 +58,3 @@ export default {
   }),
 };
 </script>
-
-<style scoped>
-
-.program_name{
-  margin: 5%;
-  font-size: 20px;
-}
-
-.toolbars{
-  padding-left: 3%;
-  padding-right: 3%;
-  padding-top: 0.5%;
-  padding-bottom: 0.5%;
-  
-  border-radius: 5px;
-  background-color: #E0E0E0;
-}
-
-.width_height_box{
-  width: 50px;
-  border-radius: 5px;
-  background-color: #FFFFFF;
-  text-align: center;
-}
-.canvas_ratio_box{
-  width: 50px;
-  border: 1px solid;
-  text-align: center;
-}
-
-.a{
-  margin: 1%;
-  border-radius: 5px;
-  background-color: #F1C40F; 
-  color: #FFFFFF;
-}
-
-.font1{
-  color: #FFFFFF;
-}
-
-.wrap{
-  margin-right: 3%;
-  display: flex;
-}
-</style>

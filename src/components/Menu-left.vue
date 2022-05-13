8cb496bee4b5967006a4fbc639ee48e7e80afdd5
Ohyoungkeun <oh-jack@hanmail.net>
2022-05-13 16:47:28 +0900

    좀 되라

---
+++ src/components/Menu-left.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container>
+      <input type = "text" name = "search" value = "검색">
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+  </v-container>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style scoped>
+div {
+  flex: 1;
+  background-color: #EEEEEE;
+
+}
+
+</style>>
---
+++ src/components/Menu-right.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container>
+    Menu Componenent 우측 메뉴
+  </v-container>  
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style scoped>
+div {
+  flex: 1; 
+  background-color: #EEEEEE;
+}
+</style>>
